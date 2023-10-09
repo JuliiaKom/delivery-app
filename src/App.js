8775e0567479beadc186178af673b03ba3ceaf26
Yuliia <julkomisar@gmail.com>
2023-10-09 10:47:57 +0300

    add Routes Women/Men/Boys

--- src/App.js
+++ src/App.js
@@ -9,16 +9,20 @@ import {Link, Route, Routes} from "react-router-dom";
 import Main from "./Components/Homepage/main";
 import Sale from "./Components/Shop/Sale";
 import Girls from "./Components/Shop/Girls";
+import Women from "./Components/Shop/Women";
+import Men from "./Components/Shop/Men";
+import Boys from "./Components/Shop/Boys";
 function App(props) {
   return (
     <div className={css.delivery}>
         <Navbar data={props.data.navbar}/>
-        {/*<Homepage data={props.data.main}/>*/}
-        {/*<Section data={props.data.section}/>*/}
         <Routes>
             <Route path="/*" element={<Main data={props.data}/>}/>
-            <Route path="/Sale" element={<Sale />}/>
+            <Route path="/Women" element={<Women />}/>
+            <Route path="/Men" element={<Men />}/>
             <Route path="/Girls" element={<Girls />}/>
+            <Route path="/Boys" element={<Boys />}/>
+            <Route path="/Sale" element={<Sale />}/>
         </Routes>
         <Footer/>
 
