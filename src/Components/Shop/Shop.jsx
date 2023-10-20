e636c5aa6c074c72ab90aa1e8dc3f7193ba5882a
Yuliia <julkomisar@gmail.com>
2023-10-21 00:19:45 +0300

    add style in the Shop.module.scss and change in useState

--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -1,46 +1,49 @@
 import React, {useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
-import {useDispatch, useSelector} from "react-redux";
-import fake_data from '../../data/shop_data.json'; // Import the JSON data
+import fake_data from '../../data/shop_data.json';
+import css from "./Shop.module.scss";
 
 const Shop = () => {
-    console.log('gdfdgdfg')
-    // let products = useSelector(state => state.shop.products)
     let {category} = useParams();
-    const [data, setData] = useState(null);
-    const [products, setProducts] = useState(fake_data.products);
+    const [products, setProducts] = useState();
 
     useEffect(() => {
-        // Simulate a fake REST API call
         const fetchData = () => {
             return Promise.resolve({
-                json: () => (products),
+                json: () => (setProducts(fake_data.products)),
             });
         };
 
         fetchData()
             .then(response => response.json())
-            .then(result => setData(result))
             .catch(error => console.error('Error:', error));
     }, []);
+    const filteredProducts = products?.filter((product) => product.category === category);
 
-    const filteredProducts = products.filter((product) => product.category === category);
-
-
+    // const ShowMoreHandler = () => {
+    //   console.log('change value')
+    // }
     return (
         <div>
             {filteredProducts && (
-                <ul>
+                <div className={css.Shop}>
                     {filteredProducts.map(product => (
-                        <li key={product.name}>
-                            {product.name}, {product.category}, {product.size}, ${product.price}
-                            <img src={product.image} alt=""/>
-                        </li>
+                        <div className={css.container} key={product.id}>
+                                <img src={product.image} alt=""/>
+                            <div className={css.containerProduct}>
+                                {product.name}, {product.size}, ${product.price}
+                                <div>
+                                    <button>Show more info</button>
+                                    <button>Add to cart</button>
+                                </div>
+
+                            </div>
+
+                        </div>
                     ))}
-                </ul>
+                </div>
             )}
         </div>
     )
 }
-
 export default Shop;
